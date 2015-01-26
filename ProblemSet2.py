02c50d344a2185636ff0e7c9d1559d0575f8cfe4
vamsi-n <vamsi.nellore@gmail.com>
2015-01-26 15:07:39 -0500

    Added calculatedFixedPayoffAmount method

--- ProblemSet2.py
+++ ProblemSet2.py
@@ -20,4 +20,27 @@ def calculateCreditCardBalance(balance, annualInterestRate, monthlyPaymentRate):
 
     print 'Total paid: ' + str(round(totalPaid,2))
 
-calculateCreditCardBalance(4213, 0.2, 0.04)
+
+def calculateFixedPayoffAmount(balance, annualInterestRate):
+
+    print 'balance = ' + str(balance)
+    print 'annualInterestRate = ' + str(annualInterestRate)
+    minimumMonthlyPayment = 100.0
+
+    original_balance = balance
+
+    while balance > 0:
+        balance = original_balance
+        for i in range(1, 13):
+            interest = (annualInterestRate/12.0) * (balance - minimumMonthlyPayment)
+            balance += (round(interest, 2) - round(minimumMonthlyPayment, 2))
+
+        print 'monthly payment : ' + str(minimumMonthlyPayment) + ' balance : ' + str(balance)
+        if balance > 0:
+            minimumMonthlyPayment += 10
+        else:
+            break
+
+    print('Lowest Payment : ' + str(minimumMonthlyPayment))
+
+calculateFixedPayoffAmount(3926, 0.2)
